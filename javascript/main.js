3edabc89bbc2fb4c060b0f571b99de843170caa6
haydynmitchell55 <haydynmitchell55@gmail.com>
2025-11-17 14:23:53 +0000

    Added a query parameter

--- javascript/main.js
+++ javascript/main.js
@@ -23,3 +23,20 @@ window.onload = () => {
     mainContent.style.display = "block";
   };
 };
+
+
+document.getElementById("searchBox").addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    const q = document.getElementById("searchBox").value;
+    window.location.href =
+      "https://www.google.com/search?q=" + encodeURIComponent(q);
+  }
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get("q");
+
+if (query) {
+  window.location.href =
+    "https://www.google.com/search?q=" + encodeURIComponent(query);
+}
